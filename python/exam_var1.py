ceb0e87d26a642069178fba4162fc8ed9db81a17
Jong Cheon Park <simpleego@gmail.com>
2024-10-16 11:07:27 +0900

    Add files via upload

---
+++ python/exam_var1.py
@@ -0,0 +1,10 @@
+a="python"  #a 변수에 "python"을 저장
+b='Hello'   #b 변수에 'Hello'를 저장
+
+print(a)
+print(b)
+print(a+b)  #a에 저장된 "python"과 b에 저장된 "Hello"문자를 붙여서 출력
+# 결과는
+# python
+# Hello
+# pythonHello
