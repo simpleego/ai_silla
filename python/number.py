4274437a64642046113aeaff2e824e7f9a65741e
Jong Cheon Park <simpleego@gmail.com>
2024-10-17 22:56:04 +0900

    Add files via upload

---
+++ python/number.py
@@ -0,0 +1,24 @@
+int_num1 = 123
+float_num1=123.1234567890123456789
+octal_num1=0o61
+hexa_num1=0xff
+exp_num=123E12
+exp_num1=123e-15
+print(exp_num1)
+
+num1=5
+num2=3
+nam=num1%num2
+print(nam)
+mok=num1//num2
+print(mok)
+num3=num1/num2
+print(num3)
+
+# 복합연산자 +=
+a=10
+a+=4
+print(a)
+b=5
+a+=b
+print(a)
---
+++ python/string.py
@@ -0,0 +1,36 @@
+#"Life is too short, You need Python"
+#"a"
+#"123"
+str1 = "Life is too short," 
+str2 = str1 + "You need Python"
+print(str2)
+str3= "You're wellcome"
+print(str3)
+
+str4 = """Life is 
+too short, 
+You need 
+python"""
+
+print(str4)
+
+str5 = '"Python is very easy." he says.'
+print(str5)
+
+food = 'Python\'s favoriten \nfood is perl'
+print(food)
+
+say = "\"Python is very easy.\" he says."
+print(say)
+
+multiline = "Life is too short\fYou need python"
+print(multiline)
+beep = "\b\b\b"
+print(beep)
+word="pytho\bn"
+print(word)
+
+python="python"
+result = python*3
+print(result)
+print(100*"*")
