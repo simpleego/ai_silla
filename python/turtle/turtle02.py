b429ca9742d67949fdac050959c345096383265c
Jong Cheon Park <simpleego@gmail.com>
2024-10-22 17:03:19 +0900

    Add files via upload

---
+++ python/turtle/turtle02.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+t.shape('turtle')
+t.right(180)
+
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+
+t.exitonclick()
+
+
+
---
+++ python/turtle/turtle04.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+t.shape('turtle')
+
+# 오각형 그리기
+# 360/5 => 
+for i in range(50):
+    t.forward(100)
+    t.right(72)
+
+t.exitonclick()
+
+
+
