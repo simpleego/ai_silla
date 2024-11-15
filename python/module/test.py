50fda3aa11f84c1d330331b4decb4a62693100b1
Jong Cheon Park <simpleego@gmail.com>
2024-11-15 17:02:16 +0900

    Add files via upload

---
+++ python/module/test.py
@@ -0,0 +1,15 @@
+import mod1
+
+sum = mod1.add(10,20)
+sub = mod1.sub(40,50)
+
+area = mod1.PI * 2
+
+print(sum)
+print(sub)
+print(area)
+
+
+
+
+
---
+++ python/module/test01.py
@@ -0,0 +1,18 @@
+from mod1 import sub
+from mod1 import add as adder
+import mod1
+# from mod1 import *
+
+sum = adder(10,20)
+sub = sub(40,50)
+
+area = mod1.PI * 2
+
+print(sum)
+print(sub)
+print(area)
+
+
+
+
+
