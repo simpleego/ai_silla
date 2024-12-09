db63cf9fc4d554cb1ba252d40366040c3e6cf8a0
Jong Cheon Park <simpleego@gmail.com>
2024-12-09 21:48:20 +0900

    Update Input.svelte

--- Web_basic/Svelte/TodoList/Input.svelte
+++ Web_basic/Svelte/TodoList/Input.svelte
@@ -5,16 +5,33 @@
 </script>
 
 
-<main>
-    <input	type="text"
-    placeholder="할일을 입력하쇼"
-    bind:value={title}
-    on:keyup={(e) => handleKeyUp(e)}
-        />
- <button on:click={addTodo}>ADD</button>
-
+<main>  
+    <div id="insertItem"> 
+        <p>To Do List</p>
+        <div>
+            <input	type="text"
+            placeholder="할일을 입력하쇼"
+            bind:value={title}
+            on:keyup={(e) => handleKeyUp(e)}
+                />
+            <button on:click={addTodo}>ADD</button>
+        </div>
+    </div> 
 </main>
 
 <style>
+    #insertItem {
+        padding-top: 30px;
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        height: 55px;
+        padding-left: 100px;
+        background-color: steelblue;
+    }
 
 </style>
