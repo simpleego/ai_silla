b51568dd3c3827545f0c4b64cd6f7c303e68eae5
Jong Cheon Park <simpleego@gmail.com>
2024-12-19 20:11:34 +0900

    Add files via upload

---
+++ Java/DataType_Operrator/CircleArea.java
@@ -0,0 +1,20 @@
+package com.simple.exam;
+
+
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        final double PI = 3.141592;
+        int radius;  // 반지름
+        double area;  // 면적(PI*radius*radius)
+
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.println("원의 반지름 : ");
+        radius = kbd.nextInt();
+        area = PI * radius * radius;
+
+        System.out.printf("원의 면적 : %,6.2f",area);
+    }
+}
---
+++ Java/DataType_Operrator/Constant.java
@@ -0,0 +1,13 @@
+package com.simple.exam;
+
+public class Constant {
+    public static void main(String[] args) {
+        final double KM_PER_MILE = 1.609344;
+        double km;
+        double mile= 60.0;
+
+        km = KM_PER_MILE * mile;
+
+        System.out.println(mile+"마일 : "+km+"킬로미터입니다.");
+    }
+}
