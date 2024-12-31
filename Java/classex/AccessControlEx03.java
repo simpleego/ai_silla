04b7cbb126b2dff455b915780d685d9fbf221143
Jong Cheon Park <simpleego@gmail.com>
2024-12-31 09:05:06 +0900

    Add files via upload

---
+++ Java/classex/AccessControlEx03.java
@@ -0,0 +1,12 @@
+package com.simple.exam.classex;
+
+import com.simple.exam.classex2.AccessControlEx01;
+
+public class AccessControlEx03 {
+    public static void main(String[] args) {
+       AccessControlEx01 obj = new AccessControlEx01();
+        obj.c = 200;
+        System.out.println(obj.c);
+
+    }
+}
